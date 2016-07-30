feb4e46737173d3265d56ecf0e942a219f66f7af
Steve Eynon <steve.eynon@alienfactory.co.uk>
2016-07-30 18:59:28 +0100

    Applied custom F4 changes

--- org.fantom.sys/src/fan/sys/BootEnv.java
+++ org.fantom.sys/src/fan/sys/BootEnv.java
@@ -366,7 +366,7 @@ public class BootEnv
     }
   }
 
-  public Class loadJavaClass(String className)
+  public Class loadJavaClass(String className, String callingPod)
     throws Exception
   {
     // handle primitives, these don't get handled by URLClassLoader
@@ -385,7 +385,13 @@ public class BootEnv
     }
 
     // route to extention classloader
-    return FanClassLoader.extClassLoader.loadClass(className);
+    return getJavaClassLoader(callingPod).loadClass(className);
+  }
+
+  @Override
+  public ClassLoader getJavaClassLoader(String pod)
+  {
+    return FanClassLoader.extClassLoader;
   }
 
 //////////////////////////////////////////////////////////////////////////
